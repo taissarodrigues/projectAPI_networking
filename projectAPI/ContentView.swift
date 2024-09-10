7781531a740747793768c79e821881389874916d
taissavitoria <taissavrp@gmail.com>
2024-09-10 08:39:12 -0300

    Initial Commit

---
+++ projectAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  projectAPI
+//
+//  Created by Taissa Vitoria Rodrigues de Paula on 10/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ projectAPI/projectAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectAPIApp.swift
+//  projectAPI
+//
+//  Created by Taissa Vitoria Rodrigues de Paula on 10/09/24.
+//
+
+import SwiftUI
+
+@main
+struct projectAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
